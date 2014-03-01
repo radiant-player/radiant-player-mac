491d385db9243bbdd2f5981d054e39853df5d8bc
Sajid Anwar <sajidanwar94@gmail.com>
2014-02-28 21:20:50 -0700

    Correct 3-finger-swipe direction, support 2-finger-swipe. Fixes #21.

--- google-music-mac/CustomWebView.h
+++ google-music-mac/CustomWebView.h
@@ -1,19 +1,24 @@
 /*
-* CustomWebView.h
-*
-* Originally created by James Fator. Modified by Sajid Anwar.
-*
-* Subject to terms and conditions in LICENSE.md.
-*
-*/
+ * CustomWebView.h
+ *
+ * Originally created by James Fator. Modified by Sajid Anwar.
+ *
+ * Subject to terms and conditions in LICENSE.md.
+ *
+ */
 
 #import <WebKit/WebKit.h>
+
+#define SWIPE_MINIMUM_LENGTH 0.3
 
 @protocol CustomWebViewDelegate
 
 @end
 
-@interface CustomWebView : WebView
+@interface CustomWebView : WebView {
+    CGFloat _swipeAmount;
+    NSMutableDictionary *_touches;
+}
 
 @property (nonatomic, strong) id<CustomWebViewDelegate> appDelegate;
 
