d53337f1f6bb8d5b514f883d4eefde67e133c06d
Walter Da Col <walter.dacol@gmail.com>
2014-03-07 01:17:20 +0100

    Added basic support for AppleScript
    Now I can use dotRemote app :P

---
+++ google-music-mac/GoogleMusicScriptCommand.m
@@ -0,0 +1,42 @@
+//
+//  GoogleMusicScriptCommand.m
+//  google-music-mac
+//
+//  Created by Walter Da Col on 3/7/14.
+//  Copyright (c) 2014 Sajid Anwar. All rights reserved.
+//
+
+#import "GoogleMusicScriptCommand.h"
+#import "AppDelegate.h"
+
+@implementation GoogleMusicScriptCommand
+
+/**
+ Called when supported applescript are executed
+ 
+ @see google-music-mac.sdef for all commands
+ @return nothing for now
+ */
+- (id)performDefaultImplementation {
+    // Retrive command
+    NSString *o_command = [[self commandDescription] commandName];
+    
+    // Retrive command param
+    //NSString *o_parameter = [self directParameter];
+    
+    AppDelegate *delegate = (AppDelegate *)[[NSApplication sharedApplication] delegate];
+    
+    if ([o_command isEqualToString:@"playpause"]){
+        [delegate playPause:self];
+    }
+    if ([o_command isEqualToString:@"back track"]){
+        [delegate backAction:self];
+    }
+    if ([o_command isEqualToString:@"next track"]){
+        [delegate forwardAction:self];
+    }
+    
+    
+    return nil;
+}
+@end
